9420c75e779fba8bebd38ec386e37edc997b5cc2
anderact <axel.chacon@tecsup.edu.pe>
2022-12-04 11:52:49 -0500

    Lab13 - Remote Config w/ Firebase

--- app/src/main/java/com/chacon/axel/poketinder/ui/view/InfoFragment.kt
+++ app/src/main/java/com/chacon/axel/poketinder/ui/view/InfoFragment.kt
@@ -3,21 +3,25 @@ package com.chacon.axel.poketinder.ui.view
 import android.os.Bundle
 import android.view.View
 import android.webkit.WebView
+import androidx.fragment.app.viewModels
 import com.chacon.axel.poketinder.databinding.FragmentInfoBinding
+import com.chacon.axel.poketinder.ui.viewmodel.InfoViewModel
 
 class InfoFragment : BaseFragment<FragmentInfoBinding>(FragmentInfoBinding::inflate) {
+
+    private val viewModel: InfoViewModel by viewModels()
 
     private lateinit var webView: WebView
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val url = "https://pokemongolive.com/es/"
-
         webView = binding.wvInfoFragment
 
         webView.settings.javaScriptEnabled = true
 
-        webView.loadUrl(url)
+        viewModel.getUrlPokemon().observe(viewLifecycleOwner) {
+            webView.loadUrl(it)
+        }
     }
 }
